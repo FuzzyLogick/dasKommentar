1ecee70269f30be17edef3e2464e3d50cb4efbb5
VigoTheCarpathian <ienjoythe_nightlife@hotmail.com>
2017-11-11 00:58:29 -0500

    Updated verbage in one of the fields, nothing important

--- dasKommentar.py
+++ dasKommentar.py
@@ -5,7 +5,7 @@ by everyone from Web Pentesters, Red Teams, HTML developers, to generally nosy G
 Why? Because sometimes devs don't do a good job cleaning up their code and leave behind helpful clues that can 
 be leveraged into additional misdeeds. The idea for this came from HackThisSite's Realistic Challenge 3.
 
-Please be aware, this script does not replace diligent HTML code source review, just merely acts as a 
+Please be aware, this script does not replace diligent HTML source code review, just merely acts as a 
 "double check" in case something of interest is overlooked.  
 
 This script depends on BeautifulSoup to run.
